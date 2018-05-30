396d97b4ab45d4d10210b3a850c72c14e88678f8
Joseph Wong <joseph.wong@d2l.com>
2018-05-30 13:33:09 -0400

    automatically fix lint issues (#9)
    * automatically fix lint issues
    
    * fix gulpfile as well

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
+/*global require */
 var gulp = require('gulp');
 var merge = require('gulp-merge-json');
 var path = require('path');
 
 gulp.task('build-locales', () => {
-    gulp.src('lang/*.json')
-    .pipe(merge({
-        fileName: 'locales.json',
-        edit: (parsedJson, file) => {
-			const lang = path.basename(file.path, '.json');
-			return {
-				[lang]: parsedJson
-			};
-        }
-    }))
-    .pipe(gulp.dest('.'));
+	gulp.src('lang/*.json')
+		.pipe(merge({
+			fileName: 'locales.json',
+			edit: (parsedJson, file) => {
+				const lang = path.basename(file.path, '.json');
+				return {
+					[lang]: parsedJson
+				};
+			}
+		}))
+		.pipe(gulp.dest('.'));
 });
